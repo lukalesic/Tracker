102e57f3af02e8537d031bcdc6b5c4a99e1b5cdd
Luka Lešić <lukalesic20@gmail.com>
2023-04-20 02:59:29 +0200

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Luka Lešić on 20.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
